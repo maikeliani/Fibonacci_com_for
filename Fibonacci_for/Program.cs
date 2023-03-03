f961c6758457a4733cccb758ac048a4a221a2339
maikeliani <maikel.iani31@gmail.com>
2023-03-03 14:05:31 -0300

    Add project files.

---
+++ Fibonacci_for/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Reflection;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        double ant = 1, prox = 0;
+        int posicao;
+
+        Console.WriteLine(" Informe a posição desejada:  ");
+        posicao = int.Parse(Console.ReadLine());
+
+        for( int i=0; i < posicao; i++)
+        {
+            Console.WriteLine(ant + prox + " ");
+            prox = ant + prox;
+            ant = prox - ant;
+            
+        }
+    }
+}
